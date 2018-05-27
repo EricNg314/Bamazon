d5f50f3d2b9a6a8f3945945b5b81043fc17bebc8
Okens <Ng.Eric314@gmail.com>
2018-05-27 01:40:36 -0700

    Updating with View Products For Sale functionality, as well as table.

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,32 +15,70 @@ var connection = Mysql.createConnection({
 
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     start();
 
 });
 
-function start(){
+function start() {
     console.log("\n -------------------------------------------------------------------- \n");
     Inquirer.prompt(
         {
             type: "list",
             name: "task",
             message: "What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Products"]
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Products", "Exit Bamazon Manager"]
         }
-    ).then(function(userInput){
+    ).then(function (userInput) {
         console.log(userInput.task);
+        switch (userInput.task) {
+            case "View Products for Sale":
+                // console.log("For Sale");
+                viewProducts();
+                break;
+            case "View Low Inventory":
+                // console.log("Items in inventory");
+                break;
+            case "Add to Inventory":
+                // console.log("Adding inventory");
+                break;
+            case "Add New Products":
+                // console.log("Adding a new product");
+                break;
+            case "Exit Bamazon Manager":
+                quitApp();
+                break;
+        };
 
-        quitApp();
     });
 }
 
+function viewProducts() {
+    connection.query("SELECT * FROM inventory", function (err, res) {
+        showSQLTable(res);
+        start();
+    })
+}
 
-function quitApp(){
+function quitApp() {
     console.log("\n ==================================================================== \n");
     console.log("Thank you for using Bamazon Manager for all your managerial needs!");
     connection.end();
 }
 
+function showSQLTable(res) {
+    var keys = Object.keys(res[0]);
+    var table = new Table({
+        head: keys
+    });
+
+    for (let i = 0; i < res.length; i++) {
+        var rowData = [];
+        for (var objKeys in res[i]) {
+            rowData.push(res[i][objKeys]);
+        }
+        table.push(rowData);
+    }
+    console.log(table.toString());
+}
